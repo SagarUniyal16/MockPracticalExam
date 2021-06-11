67c0583a9976ad14f3501eb43bbcbc5eaf9aedea
SagarUniyal16 <85723642+SagarUniyal16@users.noreply.github.com>
2021-06-11 10:23:46 +0530

    Add files via upload

---
+++ CSE-A/Numbers.java
@@ -0,0 +1,13 @@
+public class Numbers {
+    
+  public static void main(String[] args)
+    {     
+    int i;
+	System.out.println ("The numbers from 1 to 10 are:\n");
+	for (i=1;i<=10;i++)
+	{      
+		System.out.println (i);
+	}
+System.out.println ("\n");
+}
+}
